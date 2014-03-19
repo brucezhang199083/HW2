915ccf72bd7b57af62589313b84bad1d17e15fe8
zhanghao <zhanghao@f6e098cd-4e5d-4ae5-a980-35725a702259>
2014-03-19 13:52:56 +0000

    git-svn-id: svn://ds01.seas.upenn.edu:10455/zhanghao/HW2@4003 f6e098cd-4e5d-4ae5-a980-35725a702259

---
+++ src/edu/upenn/cis455/servlet/XPathServletTest.java
@@ -0,0 +1,64 @@
+package edu.upenn.cis455.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import junit.framework.TestCase;
+
+import org.mockito.Mockito;
+
+import edu.upenn.cis455.servlet.XPathServlet;
+
+public class XPathServletTest extends TestCase {
+
+
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		
+		try
+		{
+			HttpServletRequest mockRequest = Mockito.mock(HttpServletRequest.class);
+			HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			Mockito.when(mockResponse.getWriter()).thenReturn(pw);
+			XPathServlet xps = new XPathServlet();
+			xps.doGet(mockRequest,mockResponse);
+			//System.out.println(sw.toString());
+			assertTrue(sw.toString().contains("Enter URL of an HTML or XML"));
+		}
+		catch (Exception e)
+		{
+			
+		}
+	}
+
+	public void testDoPostHttpServletRequestHttpServletResponse() {
+
+		try
+		{
+			HttpServletRequest mockRequest = Mockito.mock(HttpServletRequest.class);
+			HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			Mockito.when(mockResponse.getWriter()).thenReturn(pw);
+			Mockito.when(mockRequest.getParameter("urlofxml")).thenReturn("http://www.seas.upenn.edu/~cis455/demo/example1.xml");
+			Mockito.when(mockRequest.getParameter("xpathnum0")).thenReturn("/html");
+			Mockito.when(mockRequest.getParameter("another")).thenReturn(null);
+			Mockito.when(mockRequest.getParameter("done")).thenReturn("Submit");
+			
+			XPathServlet xps = new XPathServlet();
+			xps.doPost(mockRequest,mockResponse);
+			//System.out.println(sw.toString());
+			assertTrue(sw.toString().contains("Successfully Submitted!"));
+		}
+		catch (Exception e)
+		{
+			
+		}
+	}
+
+}
